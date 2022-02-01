aaf5c7806893d263484af8749cabebdacad83078
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-02-01 12:29:02 -0500

    merge commit after update--ignore

--- Components/Home.js
+++ Components/Home.js
@@ -1,22 +1,22 @@
-import { StyleSheet, View } from 'react-native';
-import { Headline, Button } from 'react-native-paper';
+import { StyleSheet, View } from "react-native";
+import { Headline, Button } from "react-native-paper";
 
 export default function Home({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Headline>Welcome to Leashed!</Headline>
-      <Button mode="contained" onPress={() => navigation.navigate('Profile')}>
-      Profile
-      </Button>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Headline>Welcome to Leashed!</Headline>
+			<Button mode="contained" onPress={() => navigation.navigate("Profile")}>
+				Profile
+			</Button>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
