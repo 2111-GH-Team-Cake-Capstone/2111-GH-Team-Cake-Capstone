b200262941753dd5aade680846525c4c3d41532e
Tiffany McNerlin <91140961+tiffmc1@users.noreply.github.com>
2022-02-02 17:41:20 -0500

    Update App.js

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //import merge from 'deepmerge';
 
 import { AuthProvider } from "./hooks/useAuth";
+import { LogBox } from "react-native";
 
 //Components
 import Home from "./Components/Home";
