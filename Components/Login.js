be4940437a792198d744e3a31be346732852706a
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-02-02 10:35:28 -0600

    mob code

--- Components/Login.js
+++ Components/Login.js
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import { StyleSheet, View, KeyboardAvoidingView, Platform} from 'react-native';
 import { Headline, Button, TextInput } from 'react-native-paper';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
+  async function handleSubmit (){
+     const createdUser =  await createUserWithEmailAndPassword(auth, email, password)
+    // .then((userCredential) => {
+    // // Signed in
+    // const user = userCredential.user;
+    console.log("USER", createdUser)
+    // })
+    // .catch((error) => {
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // ..
+    // });
+  }
   return (
     <KeyboardAvoidingView enabled={Platform.OS === "ios"}
       style={styles.container} behavior="padding">
@@ -25,7 +40,7 @@ export default function Login({ navigation }) {
           onChangeText={text => setPassword(text)}
           secureTextEntry
         />
-        <Button style={styles.input} mode="contained" onPress={() => navigation.navigate('Home')}>
+        <Button style={styles.input} mode="contained" onPress={() => handleSubmit()}>
           sign up
         </Button>
         <Button style={styles.input} mode="outlined" onPress={() => navigation.navigate('Home')}>
