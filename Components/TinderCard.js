487b5874cdb81bb847fa7002de4f940330415478
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2022-02-02 14:07:07 -0500

    reject and accept icons positioning fixed

--- Components/TinderCard.js
+++ Components/TinderCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, ImageBackground } from "react-native";
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import Icon from 'react-native-ico';
 
-const iconHeight = 50;
-const iconWidth = 50;
+const iconHeight = 75;
+const iconWidth = 75;
 export default function TinderCard() {
   return (
     <View style={styles.container}>
@@ -21,6 +22,10 @@ export default function TinderCard() {
             <Button icon="dog">View Profile</Button>
           </Card.Actions>
         </Card>
+        <View style={styles.icons}>
+          <Icon name="cancel-button" group="material-design" height={iconHeight} width={iconWidth} color="#F72119"/>
+          <Icon name="paw-black-shape" group="coolicons" height={iconHeight} width={iconWidth} color="chartreuse"/>
+        </View>
       </ImageBackground>
     </View>
   )
@@ -68,5 +73,12 @@ const styles = StyleSheet.create({
     resizeMode: "stretch",
     padding: 0,
     margin: 0,
+  },
+  icons: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    position: "absolute",
+    bottom: "12%",
   },
 })
