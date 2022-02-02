b2a3e514dd20731d4b29750798a090592af27d78
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-02-02 11:26:37 -0500

    Chat - ignore

--- Components/Chat/ChatList.js
+++ Components/Chat/ChatList.js
@@ -1,10 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import db from "../../firebase";
+import { collection, getDocs, onSnapshot, query } from "firebase/firestore";
 
 export default function ChatList() {
+	const [matches, setMatches] = useState([]);
+
+	useEffect(() => {
+		onSnapshot(query(collection(db, "matches"), where()));
+
+		// const getMatches = async () => {
+		// 	const matchesColRef = collection(db, "matches");
+		// 	const matchesData = await getDocs(matchesColRef);
+
+		// 	setMatches(
+		// 		matchesData.docs.map((doc) => ({
+		// 			...doc.data(),
+		// 			id: doc.id,
+		// 		}))
+		// 	);
+		// };
+
+		// getMatches();
+	}, []);
+
 	return (
 		<View>
-			<Text>Chatlist...</Text>
+			<Text>
+				Chatlist...
+				{/* {matches.map((match) => {
+					return match.dog_a;
+				})} */}
+			</Text>
 		</View>
 	);
 }
--- Components/Chat/ChatScreen.js
+++ Components/Chat/ChatScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 import ChatHeader from "./ChatHeader";
+import ChatList from "./ChatList";
 
 export default function ChatScreen() {
 	return (
 		<SafeAreaView>
 			<ChatHeader title="Chat" />
+			<ChatList />
 		</SafeAreaView>
 	);
 }
