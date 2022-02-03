4cb6c9ed95e48818e801912874d076107fcfca82
solichoi <soli_choi@fitnyc.edu>
2022-02-03 17:55:46 -0500

    Home Change

--- Components/Home.js
+++ Components/Home.js
@@ -2,21 +2,24 @@ import { StyleSheet, View } from "react-native";
 import { Headline, Button } from "react-native-paper";
 
 export default function Home({ navigation }) {
-	return (
-		<View style={styles.container}>
-			<Headline>Welcome to Leashed!</Headline>
-			<Button mode="contained" onPress={() => navigation.navigate("Profile")}>
-				Profile
-			</Button>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Headline>Welcome to Leashed!</Headline>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate("ViewProfile")}
+      >
+        Profile
+      </Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
