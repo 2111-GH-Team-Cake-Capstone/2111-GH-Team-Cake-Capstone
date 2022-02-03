9d1acda2afe73df7e14c0600b33b6856846aa705
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-02-03 16:34:43 -0600

    current progress

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import Login from "./Components/Login";
 import ChatMain from "./Components/Chat/ChatMain";
 import BrowseUsers from './Components/BrowseUsers';
 import MatchProfile from "./Components/MatchProfile";
-
+import CustomNavigationBar from "./Components/CustomNavigationBar";
 
 const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
 //const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
@@ -44,7 +44,11 @@ function App() {
     <PaperProvider theme = {theme}>
       <FirebaseAuthProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{
+            header: CustomNavigationBar,
+          }}>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Profile" component={Profile} />
             <Stack.Screen name="BrowseUsers" component={BrowseUsers} />
---
+++ Components/CustomNavigationBar.js
@@ -0,0 +1,9 @@
+import { Appbar } from 'react-native-paper';
+
+export default function CustomNavigationBar() {
+  return (
+    <Appbar.Header>
+      <Appbar.Content title="Leashed" />
+    </Appbar.Header>
+  );
+}
