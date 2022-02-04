babe3f52fa322fffc221b421d2a294a93d0a8ebd
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2022-02-04 16:08:11 -0500

    swipe left function now working to create a new swipes document with users' ID info

--- Components/BrowseUsers.js
+++ Components/BrowseUsers.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ImageBackground, Text } from "react-native";
-import Swiper from 'react-native-deck-swiper';
+import Swiper, { onSwipedLeft, onSwipedRight } from 'react-native-deck-swiper';
 import Icon from 'react-native-ico';
 import TinderCard from './TinderCard';
-import { collection, doc, getDocs, onSnapshot, snapshot, query, where, firebase } from "firebase/firestore"; 
+import { collection, doc, getDocs, onSnapshot, snapshot, query, where, addDoc } from "firebase/firestore"; 
 import db from '../firebase.js';
 import { useFirebaseAuth } from "../context/FirebaseAuthContext";
 
@@ -35,10 +35,10 @@ export default function BrowseUsers({navigation}) {
         ...doc.data(),
         id: doc.id,
       }));
-      //query through the swipes collection and find all documents where our current user is dog a
-      const existingSwipesCollectionRefDogA = query(swipesCollectionRef, where("dog_a", "==", "CJnHpheCmf9UyqYP4RtV"))
-      //query through the swipes collection and find all documents where our current user is dog b
-      const existingSwipesCollectionRefDogB = query(swipesCollectionRef, where("dog_b", "==", "CJnHpheCmf9UyqYP4RtV"))
+      //query through the swipes collection and find all documents where our current user is dog a and they have already swiped on dog_b
+      const existingSwipesCollectionRefDogA = query(swipesCollectionRef, where("dog_a", "==", "CJnHpheCmf9UyqYP4RtV"), where("dog_a_swiped", "==", true))
+      //query through the swipes collection and find all documents where our current user is dog b and they have already swiped on dog_b
+      const existingSwipesCollectionRefDogB = query(swipesCollectionRef, where("dog_b", "==", "CJnHpheCmf9UyqYP4RtV"), where("dog_b_swiped", "==", true))
       onSnapshot(existingSwipesCollectionRefDogA,(snapshot) => {
         const swipesDataA = snapshot.docs.map((doc) => ({
           ...doc.data(),
@@ -70,8 +70,27 @@ export default function BrowseUsers({navigation}) {
         setUsers(unseenUsers);
       });
     });
-
   }, []);
+
+  //functions to handle swipes - 
+
+  //NEED TO ADD IN CHECK TO SEE IF SWIPE DOCUMENT BETWEEN THESE TWO USERS EXISTS
+  const swipeLeft = (cardIndex) => {
+    //swiped user is the current user dispayed on the card
+    const swipedUser = users[cardIndex];
+    //adding a new swiped document where our current dog is dog_a and swipedUser is dog_b
+    addDoc(swipesCollectionRef, {
+      dog_a: "CJnHpheCmf9UyqYP4RtV",
+      dog_b: swipedUser.id,
+      dog_a_swiped: true,
+      dog_a_liked: false
+    });
+  }
+
+  const swipeRight = (cardIndex) => {
+    const swipedUser = users[cardIndex]
+  }
+
   if(users.length <= 0) {
     return (
     <Text>loading...</Text>
@@ -92,8 +111,44 @@ export default function BrowseUsers({navigation}) {
            )
          }}
          cardIndex={0}
+         onSwipedLeft={(cardIndex) => swipeLeft(cardIndex)}
+         onSwipedRight={(cardIndex) => swipeRight(cardIndex)}
          stackSize= {3}
          stackSeparation={15}
+         animateCardOpacity
+         verticalSwipe={false}
+         overlayLabels={{
+           left: {
+             title: "NO",
+             style: {
+               label: {
+                 backgroundColor: "#F72119",
+                 color: "white",
+                 fontSize: 20
+               },
+               wrapper: {
+                 flexDirection: "column",
+                 alignItems: "flex-end",
+                 justifyContent: "flex-start",
+               }
+             }
+           },
+           right: {
+            title: "YES",
+            style: {
+              label: {
+                backgroundColor: "chartreuse",
+                color: "white",
+                fontSize: 20
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "flex-start",
+              }
+            }
+           }
+         }}
        />
        <View style={styles.icons}>
         <Icon name="cancel-button" group="material-design" height={iconHeight} width={iconWidth} color="#F72119"/>
