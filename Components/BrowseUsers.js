323b1666412f04647ea939a85dde6c2b9dff3a9c
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2022-02-05 14:10:28 -0500

    restructured the calls to get data and simplified

--- Components/BrowseUsers.js
+++ Components/BrowseUsers.js
@@ -3,12 +3,14 @@ import { StyleSheet, View, ImageBackground, Text, Pressable } from "react-native
 import Swiper, { onSwipedLeft, onSwipedRight } from 'react-native-deck-swiper';
 import Icon from 'react-native-ico';
 import TinderCard from './TinderCard';
-import { collection, doc, getDocs, onSnapshot, snapshot, query, where, addDoc } from "firebase/firestore"; 
+import { collection, doc, getDocs, snapshot, query, where, addDoc, updateDoc, getDoc } from "firebase/firestore"; 
 import db from '../firebase.js';
 import { useFirebaseAuth } from "../context/FirebaseAuthContext";
 
 const iconHeight = 75;
 const iconWidth = 75;
+
+// 
 
 //grabbing user collection from our database
 const usersCollectionRef = collection(
@@ -16,82 +18,63 @@ const usersCollectionRef = collection(
   'users'
 );
 
-//grabbing swipes collection from our database
-const swipesCollectionRef = collection(
-  db,
-  'swipes'
-);
-
+//creating swiper reference for buttons
 const swiperRef = React.createRef();
 
 export default function BrowseUsers({navigation}) {
+  const currentUser = {
+    id: "CJnHpheCmf9UyqYP4RtV",
+    name: "Zelda",
+    city_location: "New York City",
+    swipes: ["ThVYa5ykI6VubgIHxEZ1", "AjgLmtGHd9JeJM6YDqOQ"]
+  }
   const [users, setUsers] = useState([]);
   // const currUser = useFirebaseAuth(); 
 
   useEffect(async () => {
-    //query through the users collection and find all users where city = current user's city
-    const localUsersCollectionRef = query(usersCollectionRef, where("city_location", "==", "New York City"))
-    onSnapshot(localUsersCollectionRef,(snapshot) => {
-      let swipedUsers = [];
-      const allUsersData = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      //query through the swipes collection and find all documents where our current user is dog a and they have already swiped on dog_b
-      const existingSwipesCollectionRefDogA = query(swipesCollectionRef, where("dog_a", "==", "CJnHpheCmf9UyqYP4RtV"), where("dog_a_swiped", "==", true))
-      //query through the swipes collection and find all documents where our current user is dog b and they have already swiped on dog_b
-      const existingSwipesCollectionRefDogB = query(swipesCollectionRef, where("dog_b", "==", "CJnHpheCmf9UyqYP4RtV"), where("dog_b_swiped", "==", true))
-      onSnapshot(existingSwipesCollectionRefDogA,(snapshot) => {
-        const swipesDataA = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        //if the current user is dog_a, then we want to push dog_b's ID to our swipedUsers array
-        swipesDataA.forEach(doc => {
-          swipedUsers.push(doc.dog_b)
-        })
-        onSnapshot(existingSwipesCollectionRefDogB,(snapshot) => {
-          const swipesDataB = snapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id,
-          }));
-          //if the current user is dog_b, then we want to push dog_a's ID to our swipedUsers array
-          swipesDataB.forEach(doc => {
-            swipedUsers.push(doc.dog_a)
-          })
-        });
-        //filter thru all of our users and only return the user's who are not in the swipedUsers array (because we already saw those users)
-        //and filter to make sure we are not also displying our current user
-        const unseenUsers = allUsersData.filter(user => {
-          if(swipedUsers.includes(user.id) || user.id === "CJnHpheCmf9UyqYP4RtV") {
-            return false;
+    let allUsers = []
+    // get all docs in the users collection where city = current user's city
+    const localUsers = query(usersCollectionRef, (where("city_location", "==", currentUser.city_location)))
+    getDocs(localUsers)
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if(doc.id !== currentUser.id && !currentUser.swipes.includes(doc.id)) {
+            allUsers.push({ ...doc.data(), id: doc.id })
           }
-          return true;
         })
-        //set state of users to the users we have not seen
-        setUsers(unseenUsers);
-      });
-    });
+        setUsers(allUsers);
+      })
   }, []);
 
-  //functions to handle swipes - 
+  // //functions to handle swipes - 
 
-  //NEED TO ADD IN CHECK TO SEE IF SWIPE DOCUMENT BETWEEN THESE TWO USERS EXISTS
-  const swipeLeft = (cardIndex) => {
-    //swiped user is the current user dispayed on the card
-    const swipedUser = users[cardIndex];
-    //adding a new swiped document where our current dog is dog_a and swipedUser is dog_b
-    addDoc(swipesCollectionRef, {
-      dog_a: "CJnHpheCmf9UyqYP4RtV",
-      dog_b: swipedUser.id,
-      dog_a_swiped: true,
-      dog_a_liked: false
-    });
-  }
+  // //NEED TO ADD IN CHECK TO SEE IF SWIPE DOCUMENT BETWEEN THESE TWO USERS EXISTS
+  // const swipeLeft = (cardIndex) => {
+  //   //swiped user is the current user dispayed on the card
+  //   const swipedUser = users[cardIndex];
+  //   const existingSwipesCollection = query(swipesCollectionRef, where("dog_b", "==", "CJnHpheCmf9UyqYP4RtV"));
+  //   if(existingSwipesCollection.length) {
+  //     updateDoc(existingSwipesCollection[0], {
+  //       dog_b_liked: false,
+  //       dog_b_swiped: true
+  //     });
+  //     return;
+  //   }
+  //   else {
+  //     //adding a new swiped document where our current dog is dog_a and swipedUser is dog_b
+  //     addDoc(swipesCollectionRef, {
+  //       dog_a: "CJnHpheCmf9UyqYP4RtV",
+  //       dog_b: swipedUser.id,
+  //       dog_a_swiped: true,
+  //       dog_a_liked: false
+  //     });
+  //     return;
+  //   }
+  // }
 
-  const swipeRight = (cardIndex) => {
-    const swipedUser = users[cardIndex]
-  }
+  // const swipeRight = (cardIndex) => {
+  //   const swipedUser = users[cardIndex]
+  // }
 
   if(users.length <= 0) {
     return (
@@ -114,8 +97,8 @@ export default function BrowseUsers({navigation}) {
            )
          }}
          cardIndex={0}
-         onSwipedLeft={(cardIndex) => swipeLeft(cardIndex)}
-         onSwipedRight={(cardIndex) => swipeRight(cardIndex)}
+        //  onSwipedLeft={(cardIndex) => swipeLeft(cardIndex)}
+        //  onSwipedRight={(cardIndex) => swipeRight(cardIndex)}
          stackSize= {3}
          stackSeparation={15}
          animateCardOpacity
