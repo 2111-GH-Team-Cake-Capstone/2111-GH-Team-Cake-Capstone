565e24a58a9da92a97b88d6c1606532a5ed61e0b
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-02-13 15:34:36 -0600

    hard coded dims working

--- Components/ChatMessage.js
+++ Components/ChatMessage.js
@@ -9,6 +9,7 @@ import {
 	Keyboard,
 	KeyboardAvoidingView,
 	Platform,
+	Dimensions
 } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import {
@@ -70,52 +71,53 @@ export default function ChatMessage(props) {
 	};
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<ImageBackground
-				source={require("../assets/capstone_bg.gif")}
-				style={styles.bgImage}
-			>
-				<KeyboardAvoidingView
-					enabled={Platform.OS === "ios" ? "padding" : "height"}
-					style={{ flex: 1 }}
-					keyboardVerticalOffset={10}
+		<KeyboardAvoidingView
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+				style={{ flex: 1 }}
+				keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 170}
+		>
+			<SafeAreaView style={styles.container}>
+				<ImageBackground
+					source={require("../assets/capstone_bg.gif")}
+					style={styles.bgImage}
 				>
-					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-						<FlatList
-							data={messages}
-							inverted={-1}
-							style={{ paddingLeft: 4 }}
-							keyExtractor={(item) => item.id}
-							renderItem={({ item: message }) =>
-								message.sender === currentDog.uid ? (
-									<ChatSenderMessage key={message.id} message={message} />
-								) : (
-									<ChatReceiverMessage
-										key={message.id}
-										message={message}
-										user={user}
-									/>
-								)
-							}
-						/>
-					</TouchableWithoutFeedback>
 
-					<View>
-						<TextInput
-							style={styles.textInput}
-							label="Message..."
-							mode="outlined"
-							value={input}
-							onChangeText={setInput}
-							onSubmitEditing={sendMessage}
-						/>
-						<Button onPress={sendMessage} title="Send">
-							Send
-						</Button>
-					</View>
-				</KeyboardAvoidingView>
-			</ImageBackground>
-		</SafeAreaView>
+						<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+							<FlatList
+								data={messages}
+								inverted={-1}
+								style={{ paddingHorizontal: 4 }}
+								keyExtractor={(item) => item.id}
+								renderItem={({ item: message }) =>
+									message.sender === currentDog.uid ? (
+										<ChatSenderMessage key={message.id} message={message} />
+									) : (
+										<ChatReceiverMessage
+											key={message.id}
+											message={message}
+											user={user}
+										/>
+									)
+								}
+							/>
+						</TouchableWithoutFeedback>
+
+						<View>
+							<TextInput
+								style={styles.textInput}
+								label="Message..."
+								mode="outlined"
+								value={input}
+								onChangeText={setInput}
+								onSubmitEditing={sendMessage}
+							/>
+							<Button onPress={sendMessage} title="Send">
+								Send
+							</Button>
+						</View>
+				</ImageBackground>
+			</SafeAreaView>
+		</KeyboardAvoidingView>
 	);
 }
 
